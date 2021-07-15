ddc7eabf0f181d0fd38e4ca6137f06b739b79cef
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-07-15 01:32:07 -0700

    notes

---
+++ learning_workspace/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+		boolean isCold = false;
+
+		if(isCold) {
+			System.out.println("It's cold, wear a coat!");
+		} else {
+			System.out.println("Not cold!");
+		}
+
+        System.out.println("Hello World");
+    }
+}
